d44f5c13f88937ea96cf913390c4249f8f059057
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-02-23 14:57:08 +0530

    question:

---
+++ question5.sh
@@ -0,0 +1,16 @@
+#Unit Conversion
+
+read -p "Enter number of inches :" inch
+
+feet=$(($inch/12))
+
+echo "$inch inches is equal to $feet feet."
+
+read -p "Enter lenght of a rectangle in feet:" x
+read -p "Enter breadth of a rectangle in feet:" y
+
+lenght=$(($a*100/328))
+breadth=$(($b*100/328))
+
+echo "$a feet is equal to : $lenght meters"
+echo "$b feet is equal to : $breadth meters"
