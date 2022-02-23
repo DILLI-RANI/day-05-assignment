f59a25606166a32dd0c72289c62d6ae5f39bcea1
P DILLI RANI <pasupulatidillirani@gmail.com>
2022-02-23 13:03:03 +0530

    added:

---
+++ digit-numner.sh
@@ -0,0 +1,27 @@
+#Read a single digit number number and write the number in the word using case
+
+read -p "Enter a number between 0 to 9:" x
+
+case $a in
+
+        1)echo "One"
+        ;;
+        2)echo "Two"
+        ;;
+        3)echo "Three"
+        ;;
+        4)echo "Four"
+        ;;
+        5)echo "Five"
+        ;;
+        6)echo "Six"
+        ;;
+        7)echo "Seven"
+        ;;
+        8)echo "Eight"
+        ;;
+        9)echo "Nine"
+        ;;
+        *)echo "You entered number is wrong"
+
+esac
